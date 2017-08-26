68bd8cc9b4f24a474b66c4eadf17eff2530433e5
Cyrus Sethna <csethna@gmail.com>
2017-08-26 11:08:15 -0500

    Hello world
    Hello world with `pygame`

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pygame
+[Learning to make games with Python](https://inventwithpython.com/makinggames.pdf)
---
+++ hello_world.py
@@ -0,0 +1,12 @@
+import pygame, sys # import statement for pygame and sys modules
+from pygame.locals import *
+
+pygame.init() # need to call init function for pygame to work
+DISPLAYSURF = pygame.display.set_mode((400, 300)) # sets dimensions of window
+pygame.display.set_caption('Hello World!') # caption text appears in title bar
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit() # terminates the program
+    pygame.display.update()
