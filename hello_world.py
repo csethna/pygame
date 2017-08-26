f1667c4256daa685e510c430edaed22463eaa327
Cyrus Sethna <csethna@gmail.com>
2017-08-26 15:03:06 -0500

    `hello world` hacking

--- hello_world.py
+++ hello_world.py
@@ -5,7 +5,7 @@ pygame.init() # need to call init function for pygame to work
 DISPLAYSURF = pygame.display.set_mode((400, 300)) # sets dimensions of window
 pygame.display.set_caption('Hello World!') # caption text appears in title bar
 while True: # main game loop
-    for event in pygame.event.get():
+    for event in pygame.event.get(): # for loop iterates over list of event objects created by `pygame.event.get()`
         if event.type == QUIT:
             pygame.quit()
             sys.exit() # terminates the program
